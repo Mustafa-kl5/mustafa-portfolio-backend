5be688693d5da384e54efbb8b14b3c140ff16322
Mustafa <mmalowisi19@cit.just.edu.jo>
2023-09-19 01:13:31 +0300

    fix images path

--- imageService/uploadImage.js
+++ imageService/uploadImage.js
@@ -17,7 +17,7 @@ const uploadImage = async (images, Category) => {
 
     try {
       await fs.promises.writeFile(filepath, base64Data, "base64");
-      const imageURL = `http://localhost:4111/${filepath}`;
+      const imageURL = `https://portfoliosre.onrender.com/${filepath}`;
       const imagePost = new imageModel({
         fileName: imageURL,
       });
